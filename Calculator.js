90635a45deb1aaff565230e31dda5c3978eca0f3
stebbi <stefant17@ru.is>
2018-10-12 16:03:20 +0000

    added calculator that takes in a string with 2 numbers and calculates the sum of those, first itteration

---
+++ Calculator.js
@@ -0,0 +1,12 @@
+function Add(numbers){
+	int sum = 0;
+	if(numbers[0]== null){
+		return null;
+	}
+	//for (int i = 0; i<= 1; i++){
+	//	if(Integer.parseInt(numbers[i] != null){
+	//		system.out.println(numbers[i]);
+	Integer.parseInt(numbers[0]) += sum; 
+	Integer.parseInt(numbers[1]) += sum;  
+	return sum; 
+}
